8e95c0de534ec6d57079aabe7b85b0ab2815cf4c
Vladlen Schetinin <vvschetinin@gmail.com>
2023-07-27 12:22:39 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,49 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import autoprefixer from 'autoprefixer';
+import pluginPurgeCss from "@mojojoejo/vite-plugin-purgecss";
+import viteImagemin from '@vheemstra/vite-plugin-imagemin';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminWebp from 'imagemin-webp';
+import imageminPngquant from 'imagemin-pngquant';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        enter: path.resolve(__dirname, 'index.html'),
+        main: path.resolve(__dirname, 'pages/main/index.html'),
+        test: path.resolve(__dirname, 'pages/testing/index.html')
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    pluginPurgeCss(),
+    viteImagemin({
+      plugins: {
+        jpg: imageminMozjpeg(),
+        png: imageminPngquant()
+      },
+      makeWebp: {
+        plugins: {
+          jpg: imageminWebp(),
+        },
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    devSourcemap: true,
+    postcss: {
+      plugins: [
+        autoprefixer,
+      ],
+    },
+  },
+})
