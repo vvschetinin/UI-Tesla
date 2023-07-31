2fb577bba9a16a612b0bbd9bf8301768f74f5cfe
Vladlen Schetinin <vvschetinin@gmail.com>
2023-07-31 16:39:16 +0300

    Доработка мобильного меню

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,9 @@
 console.log('I am App');
+
+// ==================== Toogle Mobil Menu ======================== //
+
+const bodyPage = document.querySelector('body');
+const navToogleOpen = document.querySelector('.nav-toggle');
+navToogleOpen.onclick = function(){
+  bodyPage.classList.toggle("menu-open");
+}
