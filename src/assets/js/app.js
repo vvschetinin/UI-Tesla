14a53abe3e55257f8c257dffa4d6cc25602a2f34
Vladlen Schetinin <vvschetinin@gmail.com>
2023-08-03 21:56:26 +0300

    Add Animation

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,3 +7,21 @@ const navToogleOpen = document.querySelector('.nav-toggle');
 navToogleOpen.onclick = function(){
   bodyPage.classList.toggle("menu-open");
 }
+
+// ================= Animate Title =================== //
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('element-show');
+    }
+  });
+}
+let options = {
+  threshold: [0.5] 
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+for (let elm of elements) {
+  observer.observe(elm);
+}
